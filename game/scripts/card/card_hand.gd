d5b31e4f0b65adc6a953271eac5fac0eae1502b5
niscolas <niscolas@proton.me>
2023-01-22 13:53:43 -0300

    fix: card hand hovering

--- game/scripts/card/card_hand.gd
+++ game/scripts/card/card_hand.gd
@@ -5,9 +5,11 @@ export var _current_hovered_card_index: int = 0
 
 var _player
 
+
 func setup(player):
 	_player = player
 	hover_by_index(_current_hovered_card_index)
+
 
 func hover_previous():
 	hover_by_offset(-1)
@@ -41,25 +43,23 @@ func get_card_by_index(index: int):
 	var result: Card = get_child(index)
 	return result
 
+
 func get_current_card() -> Card:
 	var result: Card = get_card_by_index(_current_hovered_card_index)
 	return result
 
-func current_card():
-	var card: Card = get_card_by_index(_current_hovered_card_index)
-	return card
 
 func has_cards():
 	var result: bool = get_child_count() >= 1
 	return result
 
+
 func destroy_card(card):
+	remove_child(card)
 	card.queue_free()
 
-func buy_card(cost :int):
-	var current_card = get_current_card()
-	destroy_card(current_card())
+
+func buy_card(cost: int):
+	destroy_card(get_current_card())
 	hover_by_index(0)
 	_player.currency_agent.spend(cost)
-
-	
